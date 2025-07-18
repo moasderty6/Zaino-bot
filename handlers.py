88f3b4437a50f4f612d34dc0d06963847e57edf7
moasderty6 <moooody.18.18.18@gmail.com>
2025-07-18 03:12:03 +0300

    Create handlers.py

---
+++ handlers.py
@@ -0,0 +1,20 @@
+from aiogram import F
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import Router
+
+router = Router()
+
+@router.message(F.text == "/start")
+async def start_handler(message: Message):
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="📢 قناة زينو الرسمية", url="https://t.me/zainaldinmaham1")],
+            [InlineKeyboardButton(text="🌐 منتدى شبكة زينو", url="https://t.me/+qaY85ZwO0HQwOGY0")],
+            [InlineKeyboardButton(text="✉️ تواصل مع زينو", url="https://t.me/Sasam132")]
+        ]
+    )
+    await message.answer(
+        f"أهلًا وسهلًا فيك <b>{message.from_user.first_name}</b> 👋\n"
+        "اختر من الأزرار التالية للتفاعل معنا:",
+        reply_markup=keyboard
+    )
