12d1a90b3642b26d2defaecf26d11b24ca2260c3
moasderty6 <moooody.18.18.18@gmail.com>
2025-07-18 03:24:31 +0300

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,10 +1,10 @@
-import os
 from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
